32f9a3a5976882f38fdb14763a3ea8b2ff4c0d73
Simone Remoli <simoneremoli00@gmail.com>
2025-12-06 10:51:57 +0100

    Sonar

--- src/main/java/it/web/routex/controller/applicativo/LogoutController.java
+++ src/main/java/it/web/routex/controller/applicativo/LogoutController.java
@@ -1,6 +1,9 @@
 package it.web.routex.controller.applicativo;
 
+import it.web.routex.exception.LogoutException;
 import it.web.routex.utility.singleton.Credentials;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,6 +17,8 @@ public class LogoutController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
     {
+        final Logger logger = LoggerFactory.getLogger(getClass());
+
 
         try {
             // 1. Svuota il singleton delle credenziali
@@ -27,9 +32,11 @@ public class LogoutController extends HttpServlet {
 
             // 3. Torna alla home / login
             response.sendRedirect("index.jsp");
+            logger.info("Logout avvenuto correttamente");
         }catch(Exception e)
         {
-            throw new RuntimeException(e.getMessage());
+            logger.error("Logout non avvenuto correttamente", e);
+            throw new LogoutException("Errore durante il logout", e);
         }
     }
 }
---
+++ src/main/java/it/web/routex/exception/LogoutException.java
@@ -0,0 +1,8 @@
+package it.web.routex.exception;
+
+public class LogoutException extends RuntimeException {
+
+  public LogoutException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
