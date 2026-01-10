3a56a2797b11ded74923a3eafc5098f94db7586c
Simone Remoli <simoneremoli00@gmail.com>
2026-01-10 18:15:49 +0100

    Refactoring code improve structure

---
+++ Database/RouteX_users_grants.sql
@@ -0,0 +1,36 @@
+-- ===============================
+-- RouteX - Users & Permissions
+-- ===============================
+
+DROP USER IF EXISTS `login_User`@`%`;
+DROP USER IF EXISTS `traveler`@`%`;
+DROP USER IF EXISTS `admin_amministratore_routex`@`%`;
+DROP USER IF EXISTS `worker`@`%`;
+
+CREATE USER `login_User`@`%`;
+CREATE USER `traveler`@`%`;
+CREATE USER `admin_amministratore_routex`@`%`;
+CREATE USER `worker`@`%`;
+
+-- LOGIN USER
+GRANT USAGE ON *.* TO `login_User`@`%`;
+GRANT EXECUTE ON PROCEDURE `routex_update`.`getallcity` TO `login_User`@`%`;
+GRANT EXECUTE ON PROCEDURE `routex_update`.`getfermatabyid` TO `login_User`@`%`;
+GRANT EXECUTE ON PROCEDURE `routex_update`.`login_user` TO `login_User`@`%`;
+GRANT EXECUTE ON PROCEDURE `routex_update`.`restituiscistazioni` TO `login_User`@`%`;
+
+-- TRAVELER
+GRANT USAGE ON *.* TO `traveler`@`%`;
+GRANT EXECUTE ON PROCEDURE `routex_update`.`getpath` TO `traveler`@`%`;
+GRANT EXECUTE ON PROCEDURE `routex_update`.`savepayment` TO `traveler`@`%`;
+GRANT EXECUTE ON PROCEDURE `routex_update`.`saveroute` TO `traveler`@`%`;
+-- ecc...
+
+-- ADMIN
+GRANT USAGE ON *.* TO `admin_amministratore_routex`@`%`;
+GRANT EXECUTE ON PROCEDURE `routex_update`.`getallpathinfo` TO `admin_amministratore_routex`@`%`;
+GRANT EXECUTE ON PROCEDURE `routex_update`.`spcommunication` TO `admin_amministratore_routex`@`%`;
+
+-- WORKER
+GRANT USAGE ON *.* TO `worker`@`%`;
+GRANT EXECUTE ON PROCEDURE `routex_update`.`viewworkschedule` TO `worker`@`%`;
